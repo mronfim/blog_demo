873623d1ce5f1bcf2c12866b229faafb93b069e5
Matheus Barella Ronfim <mronfim@Matheuss-Mac-Pro.local>
2016-01-17 13:56:24 -0500

    Loop through posts on posts#index

--- config/initializers/friendly_id.rb
+++ config/initializers/friendly_id.rb
@@ -86,3 +86,16 @@ FriendlyId.defaults do |config|
   #   end
   # }
 end
+
+module FriendlyId
+  module Slugged
+    def should_generate_new_friendly_id?
+      return true if send(friendly_id_config.slug_column).nil? && !send(friendly_id_config.base).nil?
+
+      change = :"#{friendly_id_config.base}_changed?"
+      return true if respond_to?(change) && send(change)
+
+      false
+    end
+  end
+end
